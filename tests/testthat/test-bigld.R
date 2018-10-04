d9fcf2a52aca1ab0360016850708813aa30d9474
sunnyeesl <sunnyeesl@gmail.com>
2018-10-04 16:08:34 +0900

    add unit test

--- tests/testthat/test-bigld.R
+++ tests/testthat/test-bigld.R
@@ -14,3 +14,19 @@ test_that("findMaximumIndept", {
   test3 <- findMaximumIndept(rbind(c(1,5), c(4, 8), c(6, 10)), c(1, 3, 1))
   expect_equivalent(test3[[1]], c(2))
 })
+
+
+test_that("BigLD", {
+  testmat <- matrix(1, 10, 10)
+  testmat[1:3,]<-0
+  testmat[7:10, ]<-2
+  diag(testmat)<-0
+  testSNP <- data.frame(chrN = 1,
+                        rsID = paste("SNP", seq_len(10), sep = ""),
+                        bp = seq(1, 100, 10), stringsAsFactors = FALSE)
+  test1 <- BigLD(geno = testmat, SNPinfo = testSNP)
+  test1exp <- data.frame(chr = 1, start.index = 1, end.index = 7,
+                         start.rsID = "SNP1", end.rsID = "SNP7",
+                         start.bp = 1, end.bp = 61)
+  expect_equal(test1, test1exp)
+})
--- tests/testthat/test-clqd.R
+++ tests/testthat/test-clqd.R
@@ -5,3 +5,28 @@ test_that("newSplitCliques", {
   test2 <- newSplitCliques(cliques.bp = list(c(1, 500, 510)), gapdist = 100)
   expect_equal(test, test2)
 })
+
+test_that("genoDp", {
+  testmat <- matrix(1, 10, 10)
+  testmat[1:3,]<-0
+  testmat[7:10, ]<-2
+  diag(testmat)<-0
+  test1 <- genoDp2(testmat[,1,drop=FALSE], testmat[,2,drop=FALSE], strLD = FALSE)[1,1]
+  expect_equivalent(test1, 1)
+  test1 <- genoDp2(testmat[,1,drop=FALSE], testmat[,6,drop=FALSE], strLD = FALSE)[1,1]
+  expect_equivalent(test1, 1)
+})
+
+
+test_that("CLQD", {
+  testmat <- matrix(1, 10, 10)
+  testmat[1:3,]<-0
+  testmat[7:10, ]<-2
+  diag(testmat)<-0
+  testSNP <- data.frame(chrN = 1,
+                        rsID = paste("SNP", seq_len(10), sep = ""),
+                        bp = seq(1, 100, 10), stringsAsFactors = FALSE)
+  test1 <- CLQD(geno = testmat, SNPinfo = testSNP)
+  test1exp <- c(rep(1, 7), rep(NA, 3))
+  expect_equal(test1, test1exp)
+})
